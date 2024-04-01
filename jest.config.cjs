04cb157c11a66fec916f501f4fb01ed45df81ca4
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-04-01 00:01:31 -0600

    Fix/jest.config.cjs: sintax error.

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
     },
     moduleNameMapper: {
       '^.+\\.(jpg|jpeg|png|gif|webp|svg|css)$': 'jest-transform-stub'
-    }
+    },
     testEnvironment: 'jsdom',
     testPathIgnorePatterns: ["<rootDir>/node_modules/"]
   };
