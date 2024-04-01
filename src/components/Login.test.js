7d6a61e4a4b8cd2156469f90b3b4465bb89a2b46
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-04-01 01:08:48 -0600

    Test/Login.test.js: Implement dummy test to make sure jest is working properly

---
+++ src/components/Login.test.js
@@ -0,0 +1,21 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux'; // import the provider
+import store from '../redux/store';
+import Login from './Login';
+
+// Mock the useNavigate hook
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(), // Mock useNavigate as a jest function
+}));
+
+test('Login Should display a Loading button', () => {
+  render(
+    <Provider store={store}>
+      <Login />
+    </Provider>
+  );
+  const button = screen.getByText('Login');
+  expect(button).toBeInTheDocument();
+});
