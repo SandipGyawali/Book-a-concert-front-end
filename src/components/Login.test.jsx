49f7af9ee8bd57f505e8c8450caf35aea4ed40f6
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-04-10 22:10:46 -0600

    tests/login: import http, HttpResponse and setupServer from msw package.

--- src/components/Login.test.jsx
+++ src/components/Login.test.jsx
@@ -1,6 +1,8 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { http, HttpResponse } from 'msw';
+import { setupServer } from 'msw/node';
 import { Provider } from 'react-redux'; // import the provider
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from '../redux/store';
