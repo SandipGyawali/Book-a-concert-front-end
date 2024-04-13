dda6c16e08921437a8885ef89172e1cc1e8b6f21
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-04-12 21:54:40 -0600

    test/signup: create Signup.test.jsx file,add empty test cases and import component for testing

---
+++ src/components/Signup.test.jsx
@@ -0,0 +1,41 @@
+import Signup from './Signup';
+
+describe('Signup Component', () => {
+  it('renders without crashing', () => {
+    
+  });
+
+  it('allows users to input name', () => {
+    
+  });
+
+  it('allows users to input email', () => {
+    
+  });
+
+  it('allows users to input password', () => {
+    
+  });
+
+  it('displays loading component when userStatus is loading', () => {
+    
+  });
+
+  it('displays error message when registration fails', () => {
+    
+  });
+
+  it('displays success message when registration succeeds', () => {
+    
+  });
+
+  it('calls dispatch with correct data when signup button is clicked', () => {
+    
+  });
+
+  it('handles userStatus changes properly', () => {
+    
+  });
+
+  
+});
