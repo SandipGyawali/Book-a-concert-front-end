5e2fbb51f9540daeff2f395f3801c541e1b9774f
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-04-19 23:38:04 -0600

    test/ItemDataPanel: Write test cases without implementing thet tests.

---
+++ src/components/itemDataPanel.test.jsx
@@ -0,0 +1,48 @@
+
+it('renders without crashing', () => {
+  // Arrange
+  // Act
+  // Assert
+});
+
+it('renders the concert title', () => {
+  // Arrange
+  // Act
+  // Assert
+});
+
+it('renders the concert description', () => {
+  // Arrange
+  // Act
+  // Assert
+});
+
+it('renders the concert organizer name', () => {
+  // Arrange
+  // Act
+  // Assert
+});
+
+it('renders the concert date', () => {
+  // Arrange
+  // Act
+  // Assert
+});
+
+it('renders the concert city', () => {
+  // Arrange
+  // Act
+  // Assert
+});
+
+it('renders the concert price', () => {
+  // Arrange
+  // Act
+  // Assert
+});
+
+it('triggers handleReserveClick when Reserve button is clicked', () => {
+  // Arrange
+  // Act
+  // Assert
+});
